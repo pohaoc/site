b4751f29cc46b19d2bad5bbda865ef898a6e40cd
du00d <bupochen@bu.edu>
2023-06-04 01:16:35 -0400

    cs200

---
+++ src/routes/cs200/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Seo from "$lib/components/Seo.svelte";
+  import WritingList from "./WritingList.svelte";
+  import writing from "./writing.yaml";
+</script>
+
+<Seo
+  title="Po Hao Chen – Publications"
+  description=""
+/>
+
+<section class="layout-md py-12">
+  <h2 class="heading2">CS200: Applied Algorithmic Problem Solving</h2>
+  <WritingList data={writing.teaching} />
+</section>
+
+
